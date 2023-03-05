5c701f7368b817830f7de63f5ac1697a3cb4a618
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-05 10:34:39 -0500

    Add TaskItemComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+import { Task } from 'src/app/interfaces/task';
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent {
+
+  @Input() task!: Task;
+  @Input() index!: number;
+  constructor(private taskSrv: TaskService) {}
+
+  eliminarTarea(id: string) {
+
+  }
+}
