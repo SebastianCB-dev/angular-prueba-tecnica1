51b8f8fb722ad2fdbd31230421db37f8972eb1bb
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-05 09:50:00 -0500

    Add Task Service

---
+++ src/app/interfaces/task.ts
@@ -0,0 +1,5 @@
+export interface Task {
+  "task-description": string,
+  "_id": string,
+  "task-date": string
+}
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../interfaces/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private _tasks: Task[] = [];
 
-  constructor() { }
+  constructor() {
+    this.loadTasks();
+  }  
+
+  loadTasks() {
+    const tasks = localStorage.getItem('tasks-angular');
+    if(tasks) {
+      this._tasks = JSON.parse(tasks);
+      return;
+    }
+    this._tasks = [];
+    return;
+  }
 }
